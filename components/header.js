252c365d868941a7b9efa39abd351e332129419c
Casey <casey.oneill@unb.ca>
2022-04-28 14:46:46 -0300

    fixing header

--- components/header.js
+++ components/header.js
@@ -9,7 +9,11 @@ class Header extends HTMLElement {
 			<nav class="navbar navbar-expand-md navbar-dark bg-primary shadow">
 				<div class="container">
 					<a class="navbar-brand" href="index.html">Casey O'Neill</a>
-					<div class="collapse navbar-collapse mx-3" id="navbarSupportedContent">
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+
+					<div class="collapse navbar-collapse" id="navbarSupportedContent">
 						<ul class="navbar-nav ms-auto">
 							<li class="nav-item">
 								<a id="nav-link-projects" class="nav-link" href="projects.html">Projects</a>
@@ -20,15 +24,14 @@ class Header extends HTMLElement {
 							<li class="nav-item">
 								<a id="nav-link-contact" class="nav-link" href="contact.html">Contact</a>
 							</li>
+							<li class=nav-item>
+								<a href="https://github.com/casey-oneill/" target="_blank" class="nav-link"><h4 class="my-0"><i class="fab fa-github-square"></i></h4></a>
+							</li>
+							<li class=nav-item>
+								<a href="https://www.linkedin.com/in/caseyoneill/" target="_blank" class="nav-link"><h4 class="my-0"><i class="fab fa-linkedin"></i></h4></a>
+							</li>
 						</ul>
 					</div>
-					<div class="d-flex">
-						<a class="me-2" href="https://www.linkedin.com/in/caseyoneill/" target="_blank"><h4 class="my-0"><i class="fab fa-linkedin"></i></h4></a>
-						<a href="https://github.com/casey-oneill/" target="_blank"><h4 class="my-0"><i class="fab fa-github-square"></i></h4></a>
-					</div>
-					<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-						<span class="navbar-toggler-icon"></span>
-					</button>
 				</div>
 			</nav>
 		`);
