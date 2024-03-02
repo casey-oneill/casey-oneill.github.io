7600e8ce462d72e94f85d8d713e15029d8f384a5
casey-oneill-uwaterloo <c4oneill@uwaterloo.ca>
2024-03-02 17:34:10 -0500

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,45 +1,25 @@
-import React from 'react';
-import { Element } from 'react-scroll';
-import ExperiencesPage from './pages/ExperiencesPage';
-import ContactsPage from './pages/ContactsPage';
-import ProjectsPage from './pages/ProjectsPage';
-import BiographyPage from './pages/BiographyPage';
-import Header from './components/common/Header';
-import Footer from './components/common/Footer';
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
-	const sections = [
-		{
-			id: 'home',
-			component: <BiographyPage />,
-		},
-		{
-			id: 'experience',
-			component: <ExperiencesPage />,
-		},
-		{
-			id: 'projects',
-			component: <ProjectsPage />,
-		},
-		{
-			id: 'contact',
-			component: <ContactsPage />,
-		},
-	];
-
-	const sectionItems = sections.map((section, i) =>
-		<Element id={section.id} key={section.id} name={section.id} className={i % 2 === 0 ? 'py-5' : 'bg-light py-5'}>
-			{section.component}
-		</Element>
-	);
-
-	return (
-		<div className="app">
-			<Header />
-			{sectionItems}
-			<Footer />
-		</div>
-	);
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap/js/src/collapse'; // required for navbar collapse
-import 'bootswatch/dist/zephyr/bootstrap.min.css';
-
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<BrowserRouter>
-		<Routes>
-			<Route path="/" element={<App />} />
-		</Routes>
-	</BrowserRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
