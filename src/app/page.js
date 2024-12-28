75c06e0567ef2830a839b346ccda745f247a27c8
Casey O'Neill <c4oneill@uwaterloo.ca>
2024-12-28 16:37:39 -0400

    Basic home page and routing

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,34 @@
+import Image from "next/image";
+import { GitHub, Linkedin, Mail } from "react-feather";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello, World!</h1>
+    <div className="flex gap-10">
+      <div className="flex flex-col content-center">
+        <Image
+          src="/profile.png"
+          alt="Picture of the author"
+          className="rounded-full border border-dark"
+          width={400}
+          height={400}
+        />
+        <div className="flex justify-center py-4 gap-6">
+          <a href="mailto:casey.oneill@uwaterloo.ca"><Mail /></a>
+          <a href="https://www.linkedin.com/in/caseyoneill" target="_blank" rel="noopener noreferrer"><Linkedin /></a>
+          <a href="https://github.com/casey-oneill" target="_blank" rel="noopener noreferrer"><GitHub /></a>
+        </div>
+      </div>
+      <div className="flex flex-col gap-6">
+        <h1 className="font-display font-black text-5xl">
+          Hi, I'm Casey!
+        </h1>
+        <h2 className="font-medium text-2xl">
+          I'm a graduate student at the <span className="text-accent">University of Waterloo</span> pursuing a Master of Mathematics in Computer Science with a specialization in <span className="text-accent">Human-Computer Interaction</span>.
+        </h2>
+        <p className="text-lg">
+          This follows the completion of my undergraduate degrees from the University of New Brunswick in 2023, where I earned a Bachelor of Computer Science and a Bachelor of Arts with a major in Philosophy.
+        </p>
+      </div>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,29 @@
+"use client"
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 export default function Header() {
+  const pathname = usePathname();
+  const items = [
+    { name: 'HOME', path: '/' },
+    { name: 'ABOUT ME', path: '/about' },
+    { name: 'RESUME', path: '/resume' },
+    { name: 'PROJECTS', path: '/projects' },
+  ];
+
   return (
     <div className="bg-light py-6 px-32">
       <header className="flex justify-between">
         <a href="/" className="font-display font-medium text-3xl">
-          Casey O'Neill.
+          Casey O'Neill<span className="text-accent">.</span>
         </a>
-        <div className="flex gap-4">
-          <a href="/">Home</a>
-          <a href="#">About Me</a>
-          <a href="#">Resume</a>
-          <a href="#">Projects</a>
+        <div className="flex">
+          {items.map((item) => (
+            <Link key={item.path} href={item.path} className={`${pathname === item.path ? 'text-accent' : ''} hover:text-accent h-fit mt-auto px-3`}>
+              {item.name}
+            </Link>
+          ))}
         </div>
       </header>
     </div>
