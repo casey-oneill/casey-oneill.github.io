d1119c2b0c5c78553a4089d022753312b896bfdc
Casey O'Neill <c4oneill@uwaterloo.ca>
2025-01-07 17:40:27 -0500

    Update projects and add project images

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,8 +1,9 @@
+import { Button } from "@/components/atoms";
 import { Section } from "@/components/molecules";
 import { Page } from "@/components/templates";
 import data from "@/data";
-import { useMemo } from "react";
-import { GitHub } from "react-feather";
+import Image from "next/image";
+import React, { useMemo } from "react";
 
 export default function Projects() {
   const sections = useMemo(() => ([
@@ -10,19 +11,28 @@ export default function Projects() {
       key: "software",
       component: (
         <Section name="Software">
-          <div className="grid grid-cols-1 gap-4">
+          <div className="grid grid-cols-1 md:grid-cols-12 gap-4">
             {data.projects.software.map((item) => (
-              <div key={item.name}>
-                <p className="font-medium text-accent">
-                  {item.date}
-                </p>
-                <p className="font-medium mb-2">
-                  {item.name}
-                </p>
-                <p>
-                  {item.description}
-                </p>
-              </div>
+              <React.Fragment key={item.name}>
+                <Image
+                  src={item.image}
+                  width={200}
+                  height={200}
+                  alt={item.name}
+                  className="col-span-1 md:col-span-3 bg-primary w-full rounded-lg border border-secondary dark:border-primary"
+                />
+                <div key={item.name} className="col-span-1 md:col-span-9">
+                  <p className="font-medium text-accent">
+                    {item.date}
+                  </p>
+                  <p className="font-medium mb-2">
+                    {item.name}
+                  </p>
+                  <p>
+                    {item.description}
+                  </p>
+                </div>
+              </React.Fragment>
             ))}
           </div>
         </Section>
@@ -41,9 +51,12 @@ export default function Projects() {
                 <p className="font-medium mb-2">
                   {item.name}
                 </p>
-                <p>
+                <p className="mb-2">
                   {item.description}
                 </p>
+                <a href={item.file} download={item.filename} target="_blank">
+                  <Button>PDF</Button>
+                </a>
               </div>
             ))}
           </div>
