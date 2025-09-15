2312bdeeaab3309d8f554136ed0e4ebd2d8a0f80
Casey O'Neill <c4oneill@uwaterloo.ca>
2025-09-15 13:44:50 -0300

    Update home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
-"use client"
+"use client";
 
 import Profile from "@/assets/images/profile.png";
-import { Button } from "@/components/atoms";
+import { Button, Link } from "@/components/atoms";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { GitHub, Linkedin, Mail } from "react-feather";
@@ -11,34 +11,38 @@ export default function Home() {
 
   return (
     <div className="grid grid-cols-12 gap-4 md:gap-10">
-      <div className="col-span-12 md:col-span-4 flex flex-col content-center">
+      <div className="col-span-12 flex flex-col content-center md:col-span-4">
         <Image
           src={Profile}
           width={200}
           height={200}
           alt="Picture of the author"
           placeholder="blur"
-          className="rounded-full border border-dark dark:border-light mx-auto"
+          className="mx-auto rounded-full border border-dark dark:border-light"
         />
-        <div className="flex justify-center py-4 gap-4 md:gap-6">
-          <a href="mailto:casey.oneill@uwaterloo.ca"><Mail /></a>
-          <a href="https://www.linkedin.com/in/caseyoneill" target="_blank" rel="noopener noreferrer"><Linkedin /></a>
+        <div className="flex justify-center gap-4 py-4 md:gap-6">
+          <a href="mailto:casey.oneill@uwaterloo.ca">
+            <Mail />
+          </a>
+          <a href="https://www.linkedin.com/in/caseyoneill" target="_blank" rel="noopener noreferrer">
+            <Linkedin />
+          </a>
         </div>
       </div>
-      <div className="col-span-12 md:col-span-8 flex flex-col gap-4 md:gap-6">
-        <h1 className="font-display font-bold text-2xl">
-          Hi, I'm Casey!
-        </h1>
+      <div className="col-span-12 flex flex-col gap-4 md:col-span-8 md:gap-6">
+        <h1 className="font-display text-2xl font-bold">Hi, I'm Casey!</h1>
         <h2 className="font-medium">
-          I'm a graduate student at the <span className="text-accent">University of Waterloo</span> pursuing a Master of Mathematics in Computer Science with a specialization in <span className="text-accent">Human-Computer Interaction</span>.
+          I'm a second-year master's student in <span className="text-accent">Human-Computer Interaction</span> at the{" "}
+          <span className="text-accent">University of Waterloo</span>, supervised by <Link href="https://edithlaw.ca/index.html">Edith Law</Link>.
         </h2>
         <p className="text-base">
-          This follows the completion of my undergraduate studies at the University of New Brunswick, where I graduated in 2023 with a Bachelor of Computer Science and a Bachelor of Arts with a Major in Philosophy.
+          I completed my undergraduate studies at the University of New Brunswick, where I earned a Bachelor of Computer Science with Honours,
+          supervised by <Link href="https://hci.cs.umanitoba.ca/people/bio/daniel-rea">Daniel Rea</Link>, and a Bachelor of Arts with a Major in
+          Philosophy.
         </p>
+        <p>My recent work focuses on social robotics and the personification of conversational agents, such as voice assistants and chatbots.</p>
         <div className="flex gap-2">
-          <Button onClick={() => router.push("/resume")}>
-            Resume
-          </Button>
+          <Button onClick={() => router.push("/resume")}>Resume</Button>
           <Button onClick={() => router.push("/projects")} variant="secondary">
             Projects
           </Button>
